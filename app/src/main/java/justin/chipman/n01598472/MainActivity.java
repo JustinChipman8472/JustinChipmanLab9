1aab44f96979c76abc38f8b65c9ae58249e26585
JustinChipman8472 <n01598472@humber.ca>
2024-03-18 16:11:33 -0400

    implemented the toggle theme functionality

--- app/src/main/java/justin/chipman/n01598472/MainActivity.java
+++ app/src/main/java/justin/chipman/n01598472/MainActivity.java
@@ -3,6 +3,9 @@ package justin.chipman.n01598472;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -13,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
@@ -64,6 +68,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
             }
         });
+
+        SharedPreferences sharedPreferences = getSharedPreferences("ThemePref", MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean("DarkMode", false);
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_jus_toggle) {
+            // Handle settings action
+            toggleTheme();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -102,5 +132,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         AlertDialog alert = builder.create();
         alert.show();
     }
+
+    private void toggleTheme() {
+        // Check the current theme mode and toggle it
+        SharedPreferences sharedPreferences = getSharedPreferences("ThemePref", MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean("DarkMode", false);
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            sharedPreferences.edit().putBoolean("DarkMode", false).apply();
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            sharedPreferences.edit().putBoolean("DarkMode", true).apply();
+        }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem toggleItem = menu.findItem(R.id.action_jus_toggle);
+        MenuItem searchItem = menu.findItem(R.id.action_chi_search);
+
+        int textColor = getResources().getColor(R.color.menu_text_color);
+        SpannableString sToggle = new SpannableString(toggleItem.getTitle());
+        sToggle.setSpan(new ForegroundColorSpan(textColor), 0, sToggle.length(), 0);
+        toggleItem.setTitle(sToggle);
+
+        SpannableString sSearch = new SpannableString(searchItem.getTitle());
+        sSearch.setSpan(new ForegroundColorSpan(textColor), 0, sSearch.length(), 0);
+        searchItem.setTitle(sSearch);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
 
 }
